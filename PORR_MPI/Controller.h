e6b27efad7c8cf1fed5808f496fdc98ea4b13967
Konradszk <konradszk@gmail.com>
2018-01-13 08:17:43 +0100

    almost done

--- PORR_MPI/Controller.h
+++ PORR_MPI/Controller.h
@@ -15,11 +15,11 @@ public:
 	std::map<std::string, int> minimum(std::map<std::string, int> a, std::map<std::string, int> b);
 	std::vector<std::pair<std::string, int>>  repeats(std::list<std::string> text);
 	int getSize(void) const { return(words.size()); };
+	int sum_repeats(std::map<std::string, int> min_map);
 
 private:
 	Text words_to_check_text;
 	std::list<std::string> words;
 	
-	int sum_repeats(std::map<std::string, int> min_map);
 };
 
